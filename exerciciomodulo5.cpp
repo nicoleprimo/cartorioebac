88e66bc2dd61b7f422863fe6845dd64c3eb4fa93
Nicole Primo <120340408+nicoleprimo@users.noreply.github.com>
2023-01-13 13:24:12 -0300

    Add files via upload
    incluída a opção de sair do sistema (opção 4)

--- exerciciomodulo5.cpp
+++ exerciciomodulo5.cpp
@@ -129,8 +129,8 @@ int main ()
         printf("Escolha a opção desejada do menu: \n \n");
         printf("\t1 - Registrar nomes\n");
         printf("\t2 - Consultar nomes\n");
-        printf("\t3 - Deletar nomes\n \n \n"); 
-
+        printf("\t3 - Deletar nomes\n "); 
+		printf("\t4 - Sair do sistema \n \n");
         printf("Digite a opção: "); //fim do menu
     
         scanf("%d", &opcao); //armazenamento da escolha do usuario
@@ -149,6 +149,11 @@ int main ()
 
                 case 3:
                 deletar();
+                break;
+                
+                case 4:
+                printf("Obrigadx por utilizar nosso sistema! \n Até logo! :) \n");
+                return 0;
                 break;
 
                 default: 
